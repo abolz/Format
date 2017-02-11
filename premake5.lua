5fe9a2ec6c129afa78b0cd4ea7f35198753e8494
effzeh <effzeh@protonmail.com>
2017-02-11 14:27:39 +0100

    Update build script

--- premake5.lua
+++ premake5.lua
@@ -43,17 +43,19 @@ solution "Libs"
 --            "-ftime-report",
         }
 
---    configuration { "gmake", "Debug", "not Windows" }
---        buildoptions {
---            "-fno-omit-frame-pointer",
---            "-fsanitize=undefined",
+    configuration { "gmake", "Debug", "not Windows" }
+        buildoptions {
+            "-fno-omit-frame-pointer",
+            "-fsanitize=undefined",
+            "-fsanitize=address",
 --            "-fsanitize=memory",
 --            "-fsanitize-memory-track-origins",
---        }
---        linkoptions {
---            "-fsanitize=undefined",
+        }
+        linkoptions {
+            "-fsanitize=undefined",
+            "-fsanitize=address",
 --            "-fsanitize=memory",
---        }
+        }
 
     configuration { "vs*" }
         buildoptions {
